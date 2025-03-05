19cda60d488356c62d1ab32e81db9656db321631
Peter Morganelli <petercarlmorganelli@gmail.com>
2025-03-05 01:24:25 -0500

    had rubiks cube replace laptopt on home page

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./rubiks_cube/scene.gltf");
 
   return (
     <mesh>
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.4 : 0.75}
-        position={isMobile ? [-5, -2, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.4 : 35}
+        position={isMobile ? [-5, -2, -2.2] : [0, -1.5, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
